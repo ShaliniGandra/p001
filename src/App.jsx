cf6f6c920f1b9f3be2dd968da7bacb2e8117f9a0
ShaliniGandra <2500031987@kluniversity.in>
2026-01-02 10:04:27 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+function App(){
+return( 
+    <div className='app'>
+        <h1>Details:</h1>
+        <h2>Name: Shalini</h2>
+        <h2>Program: B.Tech-CSE</h2>
+        <h2>Section: S03</h2>
+        <h2>Course: Front End Development Frameworks And UI Engineering</h2>
+    </div>
+);
+}
+export default App;
